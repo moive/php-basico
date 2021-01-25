c08e986db52aafbf821e4f20f49f0193486030cb
Moises Velasquez <mvelasquez@pictoric.com>
2021-01-24 23:23:24 -0500

    firt practice

---
+++ operators.php
@@ -0,0 +1,16 @@
+<?php
+
+$a = 3;
+$b = 4;
+$d = "4";
+$c = $a + $b;
+$z = 5;
+// echo $c;
+
+if($z > $b && $z > $a){
+    echo 'is true';
+}else{
+    echo 'is false';
+}
+
+?>
---
+++ variables.php
@@ -0,0 +1,11 @@
+<?php
+    $age = 39;
+    $name = "Moises";
+    // echo $age;
+    $br = '</br>';
+    
+    echo $name . ' is: '. $age . ' years old' . $br;
+    echo "$name is $age year old $br";
+    echo '$name is $age year old $br';
+
+?>
