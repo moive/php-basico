10ac0c56b77c3e96d3b3d3516d8a9a122d1c767b
Moises Velasquez <mvelasquez@pictoric.com>
2021-01-30 23:57:03 -0500

    fetch api frontend

--- Views/api.php
+++ Views/api.php
@@ -1,10 +1,28 @@
-<h1 id="data-demo"></h1>
+
+<button class="btn btn-success" onclick="getCurrency('dollar')">Dollar</button>
+<button class="btn btn-danger" onclick="getCurrency('euro')">Euro</button>
+
+<div class="row">
+    <div class="col-sm-6 mt-5">
+        <ul class="list-group"></ul>
+    </div>
+</div>
 
 <script>
-    fetch('/api/data.php')
-        .then(d => d.json())
-        .then(d => document.getElementById('data-demo').innerHTML = `
-            Dollar: ${d.dollar} - Euro: ${d.euro}
-        `);
+    function getCurrency(currency) {
+        
+        fetch(`/api/${currency}`)
+            .then(d => d.json())
+            .then(data => {
+                var result = document.querySelector('.list-group');
+                result.innerHTML = '';
+
+                for (const d of data) {
+                    result.innerHTML += `
+                        <li class="list-group-item">Date: ${d.date} - Value: ${d.value}</li>
+                    `;
+                }
+            });
+    }
 
 </script>
