841e11ada0ff2e8f937f22cdc15d42aa10ffe770
Moises Velasquez <mvelasquez@pictoric.com>
2021-01-30 22:33:10 -0500

    rename api-demo to api

--- Layout/urls.php
+++ Layout/urls.php
@@ -12,7 +12,7 @@ $urls = array(
     'Colors' => '/colors.php',
     'Session' => '/session.php',
     'Register' => '/register.php',
-    'Api-demo' => '/api-demo.php',
+    'Api' => '/api.php',
     '404' => '/404.php',
 );
 
